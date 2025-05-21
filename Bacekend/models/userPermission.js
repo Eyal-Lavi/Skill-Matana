3006037ec92ef33809d57875b2b647fb09e88e37
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2025-05-21 19:38:43 +0300

    Add User Permission model

---
+++ Bacekend/models/userPermission.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+const {sequelize} = require('../utils/database');
+const User = require('./user');
+const Permission = require('./permission');
+
+const UserPermission = sequelize.define('UserPermission', {
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        // references: {
+        //     model: User, 
+        //     key: 'id'
+        // },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        field:"user_id",
+    },
+    permissionId: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        // references: {
+        //     model: Permission,
+        //     key: 'id'
+        // },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        field:"permission_id"
+    }
+}, {
+    timestamps: false,
+    tableName:"user_permission"
+});
+
+// Define Many-to-Many Relationships
+User.belongsToMany(Permission, {  // User belongs to many permissions 
+    through: UserPermission ,//through UserPermission
+    foreignKey: 'userId' // in UserPermission the foreignKey that belongs to User is 'userId'
+});
+
+Permission.belongsToMany(User, { //Permission belongs to many users 
+    through: UserPermission,  // through UserPermission
+    foreignKey: 'permissionId'// in UserPermission the foreignKey that belongs to Permission is 'permissionId'
+});
+
+module.exports = UserPermission;
