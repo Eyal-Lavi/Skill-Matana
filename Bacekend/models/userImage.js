a12e4a5030fa966dff017732d9d38e57bccaa694
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2025-05-21 19:38:31 +0300

    Add User Image model

---
+++ Bacekend/models/userImage.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const {sequelize} = require('../utils/database');
+// const User = require('./user');
+
+const UserImage = sequelize.define('UserImage', {
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        // autoIncrement: true,
+        field:'user_id',
+        // references: {
+        //     model: User, 
+        //     key: 'id'
+        // },
+        onDelete: 'CASCADE',
+        primaryKey: true
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field:'url',
+        primaryKey: true
+    },
+}
+    ,{
+        tableName: 'user_image',
+        timestamps: false,
+    }
+);
+
+
+module.exports = UserImage;
